85528610d70b0fa53b1a1ab18dedc78134a78083
Blake McCarty <blake_mccarty@yahoo.com>
2021-10-11 23:19:45 -0500

    got delete to work

--- public/assets/js/routes.js
+++ public/assets/js/routes.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = (app) => {
   fs.readFile("notes.json", "utf8", (err, data) => {
@@ -13,19 +14,18 @@ module.exports = (app) => {
 
     app.post("/api/notes", function (req, res) {
       let newNote = req.body;
+      newNote.id = uuidv4();
       notes.push(newNote);
       updateDb();
-      return console.log("Added new note: " + newNote.title);
-    });
-
-    app.get("/api/notes/:id", function (req, res) {
-      res.json(notes[req.params.id]);
+      console.log("Added new note: " + newNote.title);
+      return res.json(newNote);
     });
 
     app.delete("/api/notes/:id", function (req, res) {
       notes.splice(req.params.id, 1);
       updateDb();
       console.log("Deleted note with id " + req.params.id);
+      res.json(notes);
     });
 
     app.get("/notes", function (req, res) {
